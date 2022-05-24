5d9e670356866221f8d3a0fa2517014d4e66a9dc
hashbert <obert.chen@gmail.com>
2022-05-23 22:37:31 -0400

    fixed spike trap
    damage timing tuned

--- Stealth Puzzler/Assets/Scripts/Traps/SpikeTrap.cs
+++ Stealth Puzzler/Assets/Scripts/Traps/SpikeTrap.cs
@@ -6,12 +6,13 @@ using System;
 public class SpikeTrap : MonoBehaviour
 {
     private Animator _spikeAnim;
-    [SerializeField] private float _graceTimeBeforeFirstAttack = 0.2f;
+    [SerializeField] private float _graceTimeBeforeFirstAttack = 0.5f;
     [SerializeField] private float _graceTimeBeforeReattack;
     [SerializeField] private float _graceTimeBeforeReattackReset = 2f;
     [SerializeField] private float _timeBeforeRetract = 1f;
     [SerializeField] private float _damageAmount = 1f;
     public static event Action<float> OnDamageTaken;
+    private bool _isInTriggerZone = false;
     void Start()
     {
         _spikeAnim = GetComponent<Animator>();
@@ -22,6 +23,7 @@ public class SpikeTrap : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            _isInTriggerZone = true;
             _graceTimeBeforeReattack = _graceTimeBeforeReattackReset;
             StartCoroutine(PrepAndAttack());
         }
@@ -29,10 +31,13 @@ public class SpikeTrap : MonoBehaviour
     IEnumerator PrepAndAttack()
     {
         _spikeAnim.SetTrigger("prep");
-        yield return new WaitForSeconds(_graceTimeBeforeFirstAttack);
         _spikeAnim.SetTrigger("extend");
-        OnDamageTaken?.Invoke(_damageAmount);
-        print("damage!");
+        yield return new WaitForSeconds(_graceTimeBeforeFirstAttack);
+        if (_isInTriggerZone)
+        {
+            OnDamageTaken?.Invoke(_damageAmount);
+            print("damage!");
+        }
         yield return new WaitForSeconds(_timeBeforeRetract);
         _spikeAnim.SetTrigger("retract");
     }
@@ -48,5 +53,10 @@ public class SpikeTrap : MonoBehaviour
             _graceTimeBeforeReattack = _graceTimeBeforeReattackReset;
             StartCoroutine(PrepAndAttack());
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _isInTriggerZone = false;
     }
 }
