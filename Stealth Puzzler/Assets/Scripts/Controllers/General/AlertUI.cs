d3526a2ea445f26f9668a4a4b5715c050b6cafae
hashbert <obert.chen@gmail.com>
2022-06-02 10:03:05 -0400

    fixed alertUI style script standards

---
+++ Stealth Puzzler/Assets/Scripts/Controllers/General/AlertUI.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertUI : MonoBehaviour
+{
+    [SerializeField] private PlayerHealth _playerHealth;
+    public LayerMask WhatIsEnemy;
+    //States
+    public bool IsDead;
+    public float LowDangerRange, MedDangerRange, HighDangerRange;
+    public bool PlayerInLowDanger, PlayerInMediumDanger, PlayerInHighDanger;
+    public Camera MainCamera;
+    // Start is called before the first frame update
+    [SerializeField] private GameObject _lowDangerIcon, _medDangerIcon, _highDangerIcon;
+    private void OnEnable()
+    {
+        _playerHealth.OnDie += DisableAlertUI;
+        _playerHealth.OnDie += ToggleDeath;
+    }
+
+    private void ToggleDeath()
+    {
+        IsDead = true;
+    }
+
+    private void OnDisable()
+    {
+        _playerHealth.OnDie -= DisableAlertUI;
+        _playerHealth.OnDie -= ToggleDeath;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if Player in low danger
+        PlayerInLowDanger = Physics.CheckSphere(transform.position, LowDangerRange, WhatIsEnemy);
+
+        //Check if Player in medium danger
+        PlayerInMediumDanger = Physics.CheckSphere(transform.position, MedDangerRange, WhatIsEnemy);
+
+        //Check if Player in high danger
+        PlayerInHighDanger = Physics.CheckSphere(transform.position, HighDangerRange, WhatIsEnemy);
+        if (IsDead) return;
+        if (!PlayerInLowDanger) //No danger
+        {
+            DisableAlertUI();
+            return;
+        }
+        else if (PlayerInLowDanger && !PlayerInMediumDanger && !PlayerInHighDanger) //low danger
+        {
+            DisableAlertUI();
+            _lowDangerIcon.SetActive(true);
+            _lowDangerIcon.transform.LookAt(MainCamera.transform.position);
+            return;
+        }
+        else if (PlayerInMediumDanger && !PlayerInHighDanger) //med danger
+        {
+            DisableAlertUI();
+            _medDangerIcon.SetActive(true);
+            _medDangerIcon.transform.LookAt(MainCamera.transform.position);
+            return;
+        }
+        else if (PlayerInHighDanger) //high danger
+        {
+            DisableAlertUI();
+            _highDangerIcon.SetActive(true);
+            _highDangerIcon.transform.LookAt(MainCamera.transform.position);
+            return;
+        }
+
+    }
+
+    private void DisableAlertUI()
+    {
+        _lowDangerIcon.SetActive(false);
+        _medDangerIcon.SetActive(false);
+        _highDangerIcon.SetActive(false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, LowDangerRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, MedDangerRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, HighDangerRange);
+    }
+}
