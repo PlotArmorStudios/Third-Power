f0cf1141a20d7c886815dd702a59421ceb54f018
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-06-10 01:13:31 -0400

    Modified cutscene sound logic

---
+++ Stealth Puzzler/Assets/ScriptS/Interactables/CutsceneSounds.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CutsceneSounds : MonoBehaviour
+{
+    public void PlayCutsceneSound()
+    {
+        AkSoundEngine.PostEvent("Play_SFX_Cutscene_Arrow_Demonstartion", gameObject);
+    }
+}
