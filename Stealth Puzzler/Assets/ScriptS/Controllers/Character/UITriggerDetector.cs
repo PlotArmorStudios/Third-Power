ac698ba322ba34fb23dd2df22ba65a5912d48fc3
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-06-20 17:32:10 -0400

    Implemented UI trigger detectore

--- Stealth Puzzler/Assets/ScriptS/Controllers/Character/UITriggerDetector.cs
+++ Stealth Puzzler/Assets/ScriptS/Controllers/Character/UITriggerDetector.cs
@@ -1,13 +1,25 @@
+using System;
 using UnityEngine;
 
 public class UITriggerDetector : MonoBehaviour
 {
+    [SerializeField] private GameObject _uiObject;
+
+    private void OnTriggerExit(Collider other)
+    {
+        _uiObject.SetActive(false);
+    }
+
     private void OnTriggerStay(Collider other)
     {
         var playerForward = other.gameObject.transform.forward;
         var triggerForward = gameObject.transform.forward;
         var dot = Vector3.Dot(playerForward, triggerForward);
-        if(dot > 0)
-            Debug.Log("Show UI");
+        
+        Debug.Log("Dot: " + dot);
+        
+        if(dot > 0) _uiObject.SetActive(true);
+        else _uiObject.SetActive(false);
+        
     }
 }
