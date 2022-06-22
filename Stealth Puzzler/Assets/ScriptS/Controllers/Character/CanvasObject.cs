7d94b23399fbc4046d3794731fa65504079b4909
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-06-22 10:07:48 -0400

    Altar trigger box added

---
+++ Stealth Puzzler/Assets/ScriptS/Controllers/Character/CanvasObject.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UITrigger
+{
+    public class CanvasObject : MonoBehaviour
+    {
+        public Image _interactImage;
+        public TMP_Text InteractText;
+        public GameObject _interactPanel;
+    }
+}
--- Stealth Puzzler/Assets/ScriptS/Controllers/Character/UITriggerDetector.cs
+++ Stealth Puzzler/Assets/ScriptS/Controllers/Character/UITriggerDetector.cs
@@ -1,25 +1,52 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class UITriggerDetector : MonoBehaviour
+namespace UITrigger
 {
-    [SerializeField] private GameObject _uiObject;
-
-    private void OnTriggerExit(Collider other)
+    public class UITriggerDetector : MonoBehaviour
     {
-        _uiObject.SetActive(false);
-    }
+        private GameObject _uiObject;
+        [SerializeField] private GameObject _referenceInteractable;
+        [SerializeField] private GameObject _canvasObject;
+        [SerializeField] private string _interactableText;
 
-    private void OnTriggerStay(Collider other)
-    {
-        var playerForward = other.gameObject.transform.forward;
-        var triggerForward = gameObject.transform.forward;
-        var dot = Vector3.Dot(playerForward, triggerForward);
-        
-        Debug.Log("Dot: " + dot);
-        
-        if(dot > 0) _uiObject.SetActive(true);
-        else _uiObject.SetActive(false);
-        
+        private void OnValidate()
+        {
+            if (_referenceInteractable)
+                transform.rotation = _referenceInteractable.transform.rotation;
+        }
+
+        private void Start()
+        {
+            if (_referenceInteractable)
+                transform.rotation = _referenceInteractable.transform.rotation;
+
+            var canvas = Instantiate(_canvasObject, transform.position, Quaternion.identity);
+            canvas.transform.parent = transform;
+            canvas.GetComponent<CanvasObject>().InteractText.text = _interactableText;
+            _uiObject = canvas;
+            canvas.gameObject.SetActive(false);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            _uiObject.gameObject.SetActive(false);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            var player = other.GetComponent<Controller>();
+            if (!player) return;
+
+            var playerForward = transform.worldToLocalMatrix.MultiplyVector(player.gameObject.transform.forward);
+            var triggerForward = transform.worldToLocalMatrix.MultiplyVector(gameObject.transform.forward);
+            var dot = Vector3.Dot(playerForward, triggerForward);
+
+            Debug.Log("Dot: " + dot);
+
+            if (dot > -0.5) _uiObject.gameObject.SetActive(true);
+            else _uiObject.gameObject.SetActive(false);
+        }
     }
 }
