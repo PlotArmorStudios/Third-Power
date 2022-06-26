387629f359ab5b058f886b9f5675035a608e817c
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-06-26 11:49:47 -0400

    Fixed cutscene after code restructure

---
+++ Stealth Puzzler/Assets/Scripts/Controllers/Camera/FocalPointManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class FocalPointManager : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _focalPoints;
+
+    private CinemachineFreeLook _vCam;
+
+    private void OnEnable() => ControllerManager.OnSwitchFocalPoints += HandleSwitchFocalPoint;
+    private void OnDisable() => ControllerManager.OnSwitchFocalPoints -= HandleSwitchFocalPoint;
+
+    private void Awake()
+    {
+        _vCam = GetComponent<CinemachineFreeLook>();
+    }
+
+    public void InitializeFocalPoints(ControllerManager controllerManager)
+    {
+        _focalPoints = controllerManager.FocalPoints;
+        _vCam.Follow = _focalPoints[0];
+        _vCam.LookAt = _focalPoints[0];
+    }
+
+    private void HandleSwitchFocalPoint(int focalPoint)
+    {
+        _vCam.Follow = _focalPoints[focalPoint - 1];
+        _vCam.LookAt = _focalPoints[focalPoint - 1];
+    }
+}
