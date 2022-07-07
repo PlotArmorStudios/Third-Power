28556f355a9a259367f23facb17dadbbd6cd875f
hashbert <obert.chen@gmail.com>
2022-07-07 01:48:34 -0400

    edited BlueEyeManager script and added prefab

---
+++ Stealth Puzzler/Assets/Scripts/Interactables/Manager/BlueEyeManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BlueEyeManager : MonoBehaviour
+{
+    [SerializeField] private List<BlueEye> _blueEyeList;
+    [SerializeField] private UnityEvent _onAllBlueEyesLit;
+
+    private void OnEnable()
+    {
+        BlueEye.OnEyeHit += CheckEyes;
+    }
+
+    private void OnDisable()
+    {
+        BlueEye.OnEyeHit -= CheckEyes;
+    }
+    private void CheckEyes()
+    {
+        foreach (BlueEye _blueEye in _blueEyeList)
+        {
+            if (!_blueEye.IsLit) { return; }
+        }
+        _onAllBlueEyesLit?.Invoke();
+    }
+}
