0e8f5515aed4801c9229b509f52c8889c85304fa
hashbert <obert.chen@gmail.com>
2022-07-21 11:57:54 -0400

    changed button class to TextButton

--- Stealth Puzzler/Assets/Scripts/UI/Main Menu/TextButton.cs
+++ Stealth Puzzler/Assets/Scripts/UI/Main Menu/TextButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Button : MonoBehaviour
+public class TextButton : MonoBehaviour
 {
     [SerializeField] private TMP_Text _buttonText;
     [SerializeField] private float _onHoverTextSize;
