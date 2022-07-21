8de52eb68dc28ddce0d003df45100ba890373f2e
hashbert <obert.chen@gmail.com>
2022-07-21 13:02:31 -0400

    Update OnCreditsEnd.cs

--- Stealth Puzzler/Assets/Scripts/UI/Main Menu/OnCreditsEnd.cs
+++ Stealth Puzzler/Assets/Scripts/UI/Main Menu/OnCreditsEnd.cs
@@ -2,15 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using System;
 
 public class OnCreditsEnd : MonoBehaviour
 {
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _creditsMask;
+    [SerializeField] private InputActionReference _select;
+    private void OnEnable()
+    {
+        _select.action.started += ExitToMain;
+    }
+    private void OnDisable()
+    {
+        _select.action.started -= ExitToMain;
+    }
+
+    private void ExitToMain(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            OnCreditsEnding();
+        }
+    }
+
     public void OnCreditsEnding()
     {
         _creditsMask.SetActive(false);
         _mainMenu.SetActive(true);
-
     }
 }
