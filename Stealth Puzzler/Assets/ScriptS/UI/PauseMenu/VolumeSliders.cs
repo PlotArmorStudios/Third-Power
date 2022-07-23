011e5954a67978093c497eaa725baee73f69bc17
marksamanicomposer <marksamanicomposer@gmail.com>
2022-07-22 22:42:16 -0400

    Settings Menu
    - Created Settings Menu GameObject to Parent Settings menu sliders/text/buttons
    - Put Pause objects into Pause Menu parent gameobject for ease of switching through menus
    - duplicated save button from pause menu for Master, Music and Sound Effects buttons (to match looks). Using buttons so they can function as an instant mute button
    - Created new button for ‘Back’ (in case duplicating Save button from other menu wasn’t good)
    - Set OnClick for ‘Settings’ and ‘Back’ to disable/enable respective menus
    
    - Set sliders to range from 0 to 100 in whole numbers and default to 75
    - Created VolumeSliders script in PauseMenu folder and set up unity slider and click events
    - Checked for values being saved between scenes

---
+++ Stealth Puzzler/Assets/ScriptS/UI/PauseMenu/VolumeSliders.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSliders : MonoBehaviour
+{
+    public Slider thisSlider;
+    private float sliderVolume;
+    public string Bus;
+
+
+    void Start()
+    {
+        float VolumeRTPC;
+        int reftype = 1;
+        AkSoundEngine.GetRTPCValue(Bus, null, 0, out VolumeRTPC, ref reftype);
+        //Debug.Log(Bus + " : " + VolumeRTPC);
+
+        thisSlider.value = VolumeRTPC;
+        sliderVolume = thisSlider.value;
+    }
+
+    public void OnVolumeChange()
+    {
+            sliderVolume = thisSlider.value;
+            AkSoundEngine.SetRTPCValue(Bus, sliderVolume);
+    }
+
+    public void Mute()
+    {
+            thisSlider.value = 0;
+            AkSoundEngine.SetRTPCValue(Bus, 0);
+    }
+}
