d338a7d93cb4e4987ac9e836e5a3dba288c6b09c
hashbert <obert.chen@gmail.com>
2022-08-02 19:53:49 -0400

    fixing camera
    made zoom work with y axis

--- Stealth Puzzler/Assets/ScriptS/Controllers/Camera/CinemachineScroll.cs
+++ Stealth Puzzler/Assets/ScriptS/Controllers/Camera/CinemachineScroll.cs
@@ -32,7 +32,12 @@ public class CinemachineScroll : MonoBehaviour
     private float _midScroll;
     private float _bottomScroll;
 
+    private float _topHeight;
+    private float _midHeight;
+
     [SerializeField] private InputActionReference _scrollInput;
+    [SerializeField] private float _topHeightZoomSize = 0.75f;
+    [SerializeField] private float _midHeightZoomSize = 0.5f;
 
     private void Awake()
     {
@@ -68,50 +73,75 @@ public class CinemachineScroll : MonoBehaviour
     private void Start()
     {
         _vCam = GetComponent<CinemachineFreeLook>();
+        _topHeight = _vCam.m_Orbits[0].m_Height;
+        _midHeight = _vCam.m_Orbits[1].m_Height;
     }
 
     private void ReadScrollInput()
     {
-        var _tempTopVar = _topScroll;
-        var _tempMidVar = _midScroll;
-        var _tempBottomVar = _bottomScroll;
+        //var _tempTopVar = _topScroll;
+        var _tempMidRadiusVar = _midScroll;
+        var _tempBottomRadiusVar = _bottomScroll;
+
+        var _tempTopHeightVar = _topHeight;
+        var _tempMidHeightVar = _midHeight;
         if (_scroll < 0)
         {
-            if (_topScroll + _scrollSensitivity <= _topRigMaxScroll)
+            if (_midScroll + _scrollSensitivity <= _midRigMaxScroll)
             {
-                _topScroll += _scrollSensitivity;
+                //_topScroll += _scrollSensitivity;
                 _midScroll += _scrollSensitivity;
                 _bottomScroll += _scrollSensitivity;
-                LeanTween.value(_vCam.gameObject, SetTopCallback, _tempTopVar, _topScroll, _zoomSpeed);
-                LeanTween.value(_vCam.gameObject, SetMidCallback, _tempMidVar, _midScroll, _zoomSpeed);
-                LeanTween.value(_vCam.gameObject, SetBotCallback, _tempBottomVar, _bottomScroll, _zoomSpeed);
+                //LeanTween.value(_vCam.gameObject, SetTopCallback, _tempTopVar, _topScroll, _zoomSpeed);
+                LeanTween.value(_vCam.gameObject, SetMidRadiusCallback, _tempMidRadiusVar, _midScroll, _zoomSpeed);
+                LeanTween.value(_vCam.gameObject, SetBotRadiusCallback, _tempBottomRadiusVar, _bottomScroll, _zoomSpeed);
+
+                _topHeight = _topHeight + _topHeightZoomSize;
+                _midHeight = _midHeight + _midHeightZoomSize;
+                LeanTween.value(_vCam.gameObject, SetTopHeightCallback, _tempTopHeightVar, _topHeight, _zoomSpeed);
+                LeanTween.value(_vCam.gameObject, SetMidHeightCallback, _tempMidHeightVar, _midHeight, _zoomSpeed);
             } 
         }
 
         if (_scroll > 0)
         {
-            if (_topScroll - _scrollSensitivity >= _topRigMinScroll)
+            if (_midScroll - _scrollSensitivity >= _midRigMinScroll)
             {
-                _topScroll -= _scrollSensitivity;
+                //_topScroll -= _scrollSensitivity;
                 _midScroll -= _scrollSensitivity;
                 _bottomScroll -= _scrollSensitivity;
-                LeanTween.value(_vCam.gameObject, SetTopCallback, _tempTopVar, _topScroll, _zoomSpeed);
-                LeanTween.value(_vCam.gameObject, SetMidCallback, _tempMidVar, _midScroll, _zoomSpeed);
-                LeanTween.value(_vCam.gameObject, SetBotCallback, _tempBottomVar, _bottomScroll, _zoomSpeed);
+                //LeanTween.value(_vCam.gameObject, SetTopCallback, _tempTopVar, _topScroll, _zoomSpeed);
+                LeanTween.value(_vCam.gameObject, SetMidRadiusCallback, _tempMidRadiusVar, _midScroll, _zoomSpeed);
+                LeanTween.value(_vCam.gameObject, SetBotRadiusCallback, _tempBottomRadiusVar, _bottomScroll, _zoomSpeed);
+
+                _topHeight = _topHeight - _topHeightZoomSize;
+                _midHeight = _midHeight - _midHeightZoomSize;
+                LeanTween.value(_vCam.gameObject, SetTopHeightCallback, _tempTopHeightVar, _topHeight, _zoomSpeed);
+                LeanTween.value(_vCam.gameObject, SetMidHeightCallback, _tempMidHeightVar, _midHeight, _zoomSpeed);
             }
         }
     }
 
-    private void SetTopCallback(float c)
-    {
-        _vCam.m_Orbits[0].m_Radius = c;
-    }
-    private void SetMidCallback(float c)
+    //private void SetTopCallback(float c)
+    //{
+    //    _vCam.m_Orbits[0].m_Radius = c;
+    //}
+    private void SetMidRadiusCallback(float c)
     {
         _vCam.m_Orbits[1].m_Radius = c;
     }
-    private void SetBotCallback(float c)
+    private void SetBotRadiusCallback(float c)
     {
         _vCam.m_Orbits[2].m_Radius = c;
     }
+
+    private void SetTopHeightCallback(float c)
+    {
+        _vCam.m_Orbits[0].m_Height = c;
+    }
+    private void SetMidHeightCallback(float c)
+    {
+        _vCam.m_Orbits[1].m_Height = c;
+    }
+
 }
