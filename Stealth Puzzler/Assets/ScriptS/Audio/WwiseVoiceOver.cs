d1d4eddeb8ff3a5d678a36f67583f885aae3eda4
marksamanicomposer <marksamanicomposer@gmail.com>
2022-08-04 16:30:12 -0400

    Added VOs
    - Added VOs to Wwise project
    - Added WwiseVoiceOver script and VO_Object in Final Level as example of how to trigger VO

---
+++ Stealth Puzzler/Assets/ScriptS/Audio/WwiseVoiceOver.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class WwiseVoiceOver : MonoBehaviour
+{
+    public AK.Wwise.Event VO1;
+
+    private bool _firstTime = true;
+
+    void OnTriggerEnter()
+    {
+        if (_firstTime)
+        {
+            VO1.Post(gameObject);
+            _firstTime = false;
+        }
+    }
+}
