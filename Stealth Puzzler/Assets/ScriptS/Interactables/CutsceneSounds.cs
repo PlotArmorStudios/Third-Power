4b30b391ca6c6519b91a7131557d7d91b9792737
Harleen Singh <harleen.singh0427@gmail.com>
2022-08-12 22:53:39 +0100

    Update CutsceneSounds.cs

--- Stealth Puzzler/Assets/ScriptS/Interactables/CutsceneSounds.cs
+++ Stealth Puzzler/Assets/ScriptS/Interactables/CutsceneSounds.cs
@@ -5,6 +5,5 @@ public class CutsceneSounds : MonoBehaviour
     public void PlayCutsceneSound()
     {
         AkSoundEngine.PostEvent("Play_SFX_Cutscene_Arrow_Demonstartion", gameObject);
-
     }
 }
