88986e1c1fcdee03533769ba6f1100170feb1f28
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-08-31 18:31:10 -0400

    fixed blob jitter

--- Stealth Puzzler/Assets/BlobShadowScaler.cs
+++ Stealth Puzzler/Assets/BlobShadowScaler.cs
@@ -6,33 +6,41 @@ using UnityEngine.Rendering.Universal;
 
 public class BlobShadowScaler : MonoBehaviour
 {
+    [SerializeField] private DecalProjector _decal;
     [SerializeField] private float _groundDistanceScale = 1;
-    private PlayerController _player;
-    private DecalProjector _decal;
+    [SerializeField] private Transform _feet;
     private Vector3 _height;
 
-    private void OnEnable()
+    private void Update()
     {
-        _decal = GetComponent<DecalProjector>();
-        _player = FindObjectOfType<PlayerController>();
+        var position = _feet.position;
+        position.y = _height.y;
+        _decal.transform.position = position;
+        
+        var playerDistanceToGround = Vector3.Distance(_feet.position, _height);
+        if (playerDistanceToGround < .1f)
+            return;
+        
+        Debug.Log("Player distance to ground is: " + playerDistanceToGround);
+        if (playerDistanceToGround < .45f)
+            playerDistanceToGround = .4f;
+        
+        var distanceScale = _groundDistanceScale / playerDistanceToGround;
+        Debug.Log("Distance scale is: " + distanceScale);
+        _decal.transform.localScale = new Vector3(distanceScale, distanceScale, distanceScale);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         RaycastHit hit;
         Ray ray = new Ray(transform.position, Vector3.down);
 
-        if (Physics.Raycast(ray, out hit, 1000)) _height = hit.point;
-    
-        var position = transform.position;
-        position.y = _height.y;
-        transform.position = position;
-        
-        // var playerDistanceToGround = Vector3.Distance(_player.transform.position, _height);
-        // if (playerDistanceToGround < .1f)
-        //     return;
-        //
-        // var distanceScale = _groundDistanceScale / playerDistanceToGround;
-        // transform.localScale = new Vector3(distanceScale, distanceScale, distanceScale);
+        if (Physics.Raycast(ray, out hit)) _height = hit.point;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_height, .5f);
     }
 }
