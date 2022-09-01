bccc26a94aae4ba992583139c1c69b2e956a785b
marksamanicomposer <marksamanicomposer@gmail.com>
2022-08-31 21:52:13 -0400

    Made FinalRoomMusicObject persist through scene change
    - Did title and also added MainMenuCheck() (from FinalRoomMusicObject) to GameManager

--- Stealth Puzzler/Assets/ScriptS/Music/FinalRoomBankLoader.cs
+++ Stealth Puzzler/Assets/ScriptS/Music/FinalRoomBankLoader.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class FinalRoomBankLoader : MonoBehaviour
 {
+    public static FinalRoomBankLoader Instance;
+
     public AK.Wwise.Event Play_Cymbals;
 
     void Awake()
     {
         AkBankManager.LoadBank("Final_Level_Music_Bank", false, false);
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void Start()
     {
         Play_Cymbals.Post(gameObject);
+    }
+
+    public void MainMenuCheck()
+    {
+        if (SceneManager.GetActiveScene().name == "Main Menu")
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Final Level Bank Destroyed");
+        }
     }
 }
